71b7b3fdb8d46f099edea4e1e741bc0be566a6c3
kamilzed <kamilsw13@gmail.com>
2026-03-25 09:16:59 +0100

    ficx

--- Leon/StreetMap/StreetMap/Program.cs
+++ Leon/StreetMap/StreetMap/Program.cs
@@ -104,7 +104,8 @@ foreach (var line in lines)
             var element = json.RootElement[0];
             var lat = element.GetProperty("lat").GetString();
             var lon = element.GetProperty("lon").GetString();
-            sw.WriteLine($"{city} -> {lat}, {lon}");
+            Console.WriteLine(lat);
+            sw.WriteLine($"{city},Polska,{lat.ToString(System.Globalization.CultureInfo.InvariantCulture)},{lon.ToString(System.Globalization.CultureInfo.InvariantCulture)}");
         }
         else
         {
