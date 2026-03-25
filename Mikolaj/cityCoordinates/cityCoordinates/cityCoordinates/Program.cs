b660d0745a5ed38b7239b42c11a1b4362681a565
mSajewiczz <mikolaj.sajewicz11@gmail.com>
2026-03-25 10:27:44 +0100

    code fetches data from api

--- Mikolaj/cityCoordinates/cityCoordinates/cityCoordinates/Program.cs
+++ Mikolaj/cityCoordinates/cityCoordinates/cityCoordinates/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System.Text.Json;
+
+string content = File.ReadAllText("miasta.txt");
+string query = "Warszawa";
+string url = $"https://nominatim.openstreetmap.org/search?q={Uri.EscapeDataString(query)}&format=json&polygon_kml=1&addressdetails=1";
+string mail = "";
+
+using HttpClient client = new HttpClient();
+
+client.DefaultRequestHeaders.Add("User-Agent", "MyGeoApp/1.0 (j.sjahw@gmail.com)");
+
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+
+string json = await response.Content.ReadAsStringAsync();
+var results = JsonDocument.Parse(json);
+
+foreach (var place in results.RootElement.EnumerateArray())
+{
+    string displayName = place.GetProperty("display_name").GetString();
+    string lat = place.GetProperty("lat").GetString();
+    string lon = place.GetProperty("lon").GetString();
+    Console.WriteLine($"{displayName} | lat: {lat}, lon: {lon}");
+}
