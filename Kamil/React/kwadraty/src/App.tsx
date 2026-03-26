1ddeb95c37897d9dd3dbf79c3804770e980d0950
kamilzed <kamilsw13@gmail.com>
2026-03-26 10:24:36 +0100

    rac React

---
+++ Kamil/React/kwadraty/src/App.tsx
@@ -0,0 +1,39 @@
+import { use, useState } from "react";
+import "./App.css";
+import Box from "./component/Box";
+
+function App() {
+  const [l, setL] = useState(0.5); 
+  const [counter, setCounter] = useState(10); 
+
+  const squarePoints = [
+    { x: 0, y: 0 },
+    { x: 200, y: 0 },
+    { x: 200, y: 200 },
+    { x: 0, y: 200 }
+  ];
+    const rectanglePoints = [
+    { x: 25, y: 0 },
+    { x: 175, y: 0 },
+    { x: 175, y: 200 },
+    { x: 25, y: 200 }
+  ];
+
+  return (
+    <section>
+      <p>depth = {counter}</p>
+      <p>l = {l}</p>
+      <Box counter={counter} points={squarePoints} l={l} />
+      <label htmlFor=""> 
+        larp
+        <input type="range" min="0" max="1" step="0.001" value={l} onChange={(e) => setL(parseFloat(e.target.value))} />
+      </label>
+      <label htmlFor=""> 
+        depth
+        <input type="range" min="1" max="100" step="1" value={counter} onChange={(e) => setCounter(parseInt(e.target.value))} />
+      </label>
+    </section>
+  );
+}
+
+export default App;
---
+++ Kamil/React/kwadraty/src/component/Box.tsx
@@ -0,0 +1,58 @@
+
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+function createNewPoints(points: Point[], l: number): Point[] {
+  let newPoints : Point[]= [];
+  for(let i = 0; i < 4; i++)
+  {
+    //for parent sqrt
+    let A = points[i];
+    let B = points[(i + 1) % 4];
+    let X = A.x - B.x;
+    let Y = A.y - B.y;
+    let R = Math.sqrt(X * X + Y * Y );
+
+    let r = R*l;
+    let rad = Math.atan2(Y,X);
+    let y = r * Math.sin(rad);
+    let x = r * Math.cos(rad);
+
+    newPoints.push({x : x  + B.x, y: y + B.y});
+  }
+
+  return newPoints;
+};
+
+
+
+function Box(props: {counter: number,l : number, points?: Point[]}) {
+  const l = props.l; // procent przesunięcia, możesz zmienić
+  const punkty: Point[] = [
+    { x: 0, y: 0 },
+    { x: 50, y: 0 },
+    { x: 50, y: 100 },
+    { x: 0, y: 100 },
+  ];
+  const currentPoints = props.points || punkty;
+  const nowePunkty = createNewPoints(currentPoints, l); 
+
+  if (props.counter <= 0) return <></>;
+
+  const pointsString = currentPoints.map(p => `${p.x},${p.y}`).join(' ');
+
+  return (
+    <svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+    >
+      <polygon points={pointsString} fill="none" stroke="black" strokeWidth="1" />      
+      <Box  counter={props.counter - 1} points={nowePunkty} l={l} />
+    </svg>);}
+
+export default Box;
+
