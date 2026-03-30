a3da3d42f59141ed122b36999025e07e620919cd
mSajewiczz <mikolaj.sajewicz11@gmail.com>
2026-03-30 13:44:16 +0200

    quick fix

---
+++ Mikolaj/KSeF/KSeF/KSeF/Models/Podmiot.cs
@@ -0,0 +1,10 @@
+﻿namespace KSeF.Models
+{
+    public class Podmiot
+    {
+        public string Nip { get; set; }
+        public string Nazwa { get; set; }
+        public string AdresL1 { get; set; }
+        public string KodKraju { get; set; }
+    }
+}
