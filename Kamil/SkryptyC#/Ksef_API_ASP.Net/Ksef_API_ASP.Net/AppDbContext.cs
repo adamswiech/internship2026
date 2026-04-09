3b2fee8a3b0e8bc6d1b20df827dc865357795652
kamilzed <kamilsw13@gmail.com>
2026-04-09 14:10:28 +0200

    fix

---
+++ Kamil/SkryptyC#/Ksef_API_ASP.Net/Ksef_API_ASP.Net/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ksef.Models;
+
+public class AppDbContext : DbContext
+{
+    //public DbSet<Faktura> Faktura { get; set; }
+    public DbSet<Podmiot> Podmiot{ get; set; }
+    public DbSet<FaWiersz> FaWiersz { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+}
