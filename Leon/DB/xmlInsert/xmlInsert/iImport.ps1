88b9e4413505a9c4761e0195cd9dea6de21b3701
Usern-4me <mazurkiewicz.leon@gmail.com>
2026-04-10 14:47:01 +0200

    fix powershell

--- Leon/DB/xmlInsert/xmlInsert/iImport.ps1
+++ Leon/DB/xmlInsert/xmlInsert/iImport.ps1
@@ -1,32 +1,51 @@
-[xml]$xml = Get-Content "Data.xml"
+# slow, one by one with transaction
+
+[xml]$xml = Get-Content "DataXXS.xml"
 
 $connectionString = "Server=(localdb)\MSSQLLocalDB;Database=InterDB;Integrated Security=True"
 
-$connection = New-Object System.Data.SqlClient.SqlConnection
-$connection.ConnectionString = $connectionString
+$connection = New-Object System.Data.SqlClient.SqlConnection $connectionString
+$connection.Open()
 
-using($connection) {
-    $connection.Open()
-    # Clear table
-    #$cmd = $connection.CreateCommand()
-    #$cmd.CommandText = "TRUNCATE TABLE dbo.people"
-    #$cmd.ExecuteNonQuery()
+Write-Host "Connected to database."
 
+$transaction = $connection.BeginTransaction()
+
+try {
     foreach ($p in $xml.People.Person) {
 
-        $sql = @"
-    INSERT INTO dbo.people
-    (id, first_name, middle_name, last_name, age, height_cm, weight_kg, city, country, favorite_number)
-    VALUES
-    ($($p.id), '$($p.Name.first_name)', '$($p.Name.middle_name)', '$($p.Name.last_name)',
-     $($p.Stats.age), $($p.Stats.height_cm), $($p.Stats.weight_kg),
-     '$($p.Location.city)', '$($p.Location.country)', $($p.Stats.favorite_number))
-    "@
-
         $cmd = $connection.CreateCommand()
-        $cmd.CommandText = $sql
+        $cmd.Transaction = $transaction
+
+        $cmd.CommandText = @"
+INSERT INTO dbo.people
+(first_name, middle_name, last_name, age, height_cm, weight_kg, city, country, favorite_number)
+VALUES
+(@first_name, @middle_name, @last_name, @age, @height_cm, @weight_kg, @city, @country, @favorite_number)
+"@
+
+        $cmd.Parameters.AddWithValue("@first_name", [string]$p.Name.first_name) | Out-Null
+        $cmd.Parameters.AddWithValue("@middle_name", [string]$p.Name.middle_name) | Out-Null
+        $cmd.Parameters.AddWithValue("@last_name", [string]$p.Name.last_name) | Out-Null
+        $cmd.Parameters.AddWithValue("@age", [int]$p.Stats.age) | Out-Null
+        $cmd.Parameters.AddWithValue("@height_cm", [decimal]$p.Stats.height_cm) | Out-Null
+        $cmd.Parameters.AddWithValue("@weight_kg", [decimal]$p.Stats.weight_kg) | Out-Null
+        $cmd.Parameters.AddWithValue("@city", [string]$p.Location.city) | Out-Null
+        $cmd.Parameters.AddWithValue("@country", [string]$p.Location.country) | Out-Null
+        $cmd.Parameters.AddWithValue("@favorite_number", [int]$p.Stats.favorite_number) | Out-Null
+
         $cmd.ExecuteNonQuery()
     }
 
+    $transaction.Commit()
+    Write-Host "inserted successfully."
 }
-
+catch {
+    $transaction.Rollback()
+    Write-Host "Error"
+    throw
+}
+finally {
+    $connection.Close()
+    Write-Host "Connection closed."
+}
