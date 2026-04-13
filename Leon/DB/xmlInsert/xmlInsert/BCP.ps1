57130c89430a4a6f0d2d1fdeeb1395324bfb4407
Usern-4me <mazurkiewicz.leon@gmail.com>
2026-04-13 10:08:52 +0200

    fix

--- Leon/DB/xmlInsert/xmlInsert/BCP.ps1
+++ Leon/DB/xmlInsert/xmlInsert/BCP.ps1
@@ -1,10 +1,12 @@
 
 sqllocaldb start MSSQLLocalDB
-
 $info = sqllocaldb info MSSQLLocalDB
 
 
-$pipe = ($info | Select-String "Instance pipe name").ToString().Split(":")[1].Trim()
+$pipe1 = ($info | Select-String "Instance pipe name").ToString().Split(":")[1].Trim()
+$pipe2 = ($info | Select-String "Instance pipe name").ToString().Split(":")[2].Trim()
+$pipe = $pipe1 + ":" + $pipe2
+write-host "Using pipe: $pipe"
 
 bcp "InterDB.dbo.people" in "Data.csv" -S $pipe -T -c -t ";"
 
