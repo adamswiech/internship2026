edccfabe352030708c83ac5c665b9a4ea3cd3fd9
mSajewiczz <mikolaj.sajewicz11@gmail.com>
2026-04-13 11:33:10 +0200

    quick fix

--- Mikolaj/csvConverter/csvConverter/Program.cs
+++ Mikolaj/csvConverter/csvConverter/Program.cs
@@ -2,79 +2,82 @@
 using System.Diagnostics;
 
 string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=mikolaj_db;Integrated Security=True;TrustServerCertificate=True;";
-//CSV_converter converter = new CSV_converter(connectionString);
+CSV_converter converter = new CSV_converter(connectionString);
 
 XML_converter xmlConverter = new XML_converter(connectionString);
 
-//PersonalDataModel personalDataModel = new PersonalDataModel();
-//void multithreadingCSV()
-//{
-//    List<Thread> threads = new List<Thread>();
-//    List<string> filesPaths = new List<string>();
-//    Stopwatch stopwatch = new Stopwatch();
+PersonalDataModel personalDataModel = new PersonalDataModel();
+void multithreadingCSV()
+{
+    List<Thread> threads = new List<Thread>();
+    List<string> filesPaths = new List<string>();
+    Stopwatch stopwatch = new Stopwatch();
 
-//    int threadsCount = 10; //MAIN THREAD + EVERY ADDITIONAL THREAD
-//    long elementsCount = converter.countElements() / threadsCount;
-//    stopwatch.Start();
+    int threadsCount = 10; //MAIN THREAD + EVERY ADDITIONAL THREAD
+    long elementsCount = converter.countElements() / threadsCount;
+    stopwatch.Start();
 
-//    Console.WriteLine("Timer started!");
+    Console.WriteLine("Timer started!");
 
-//    converter.fetchData(0, elementsCount, 1);
-//    filesPaths.Add($"{Path.GetFullPath($@"..\..\..\out1.csv")}");
+    converter.fetchData(0, elementsCount, 1);
+    filesPaths.Add($"{Path.GetFullPath($@"..\..\..\out1.csv")}");
 
-//    for (int i = 1; i <= (threadsCount - 1); i++)
-//    {
-//        int localCounter = i;
-//        filesPaths.Add($"{Path.GetFullPath($@"..\..\..\out{localCounter}.csv")}");
+    for (int i = 1; i <= (threadsCount - 1); i++)
+    {
+        int localCounter = i;
+        filesPaths.Add($"{Path.GetFullPath($@"..\..\..\out{localCounter}.csv")}");
 
-//        CSV_converter converter_t = new CSV_converter(connectionString);
-//        Thread t = new Thread(() => converter_t.fetchData(elementsCount * localCounter, elementsCount, localCounter + 1));
+        CSV_converter converter_t = new CSV_converter(connectionString);
+        Thread t = new Thread(() => converter_t.fetchData(elementsCount * localCounter, elementsCount, localCounter + 1));
 
-//        threads.Add(t);
-//        t.Start();
-//    }
+        threads.Add(t);
+        t.Start();
+    }
 
-//    foreach (var t in threads)
-//    {
-//        t.Join();
-//    }
+    foreach (var t in threads)
+    {
+        t.Join();
+    }
 
-//    void mergeFilesCSV()
-//    {
-//        string destFilePath = Path.GetFullPath(@"..\..\..\output.csv");
-//        File.WriteAllText(destFilePath, "");
+    void mergeFilesCSV()
+    {
+        string destFilePath = Path.GetFullPath(@"..\..\..\output.csv");
+        File.WriteAllText(destFilePath, "");
 
-//        using (TextWriter tw = new StreamWriter(destFilePath, true))
-//        {
-//            foreach (string filePath in filesPaths)
-//            {
-//                using (TextReader tr = new StreamReader(filePath))
-//                {
-//                    tw.WriteLine(tr.ReadToEnd());
-//                    tr.Close();
-//                    tr.Dispose();
-//                }
+        using (TextWriter tw = new StreamWriter(destFilePath, true))
+        {
+            foreach (string filePath in filesPaths)
+            {
+                using (TextReader tr = new StreamReader(filePath))
+                {
+                    tw.WriteLine(tr.ReadToEnd());
+                    tr.Close();
+                    tr.Dispose();
+                }
 
-//                Console.WriteLine("File Processed : " + filePath);
-//            }
+                Console.WriteLine("File Processed : " + filePath);
+            }
 
-//            tw.Close();
-//            tw.Dispose();
-//        }
-//    }
+            tw.Close();
+            tw.Dispose();
+        }
+    }
 
-//    mergeFilesCSV();
+    mergeFilesCSV();
 
-//    stopwatch.Stop();
-//    TimeSpan elapsedTime = stopwatch.Elapsed;
+    stopwatch.Stop();
+    TimeSpan elapsedTime = stopwatch.Elapsed;
 
-//    Console.WriteLine($"Elapsed time for {threadsCount} threads: {elapsedTime}");
-//}
+    Console.WriteLine($"Elapsed time for {threadsCount} threads: {elapsedTime}");
+}
 
 Stopwatch stopwatch = new Stopwatch();
 stopwatch.Start();
 
-await xmlConverter.LoadXML(@"C:\Users\Vulcan\source\repos\internship2026\Mikolaj\csvConverter\csvConverter\test.xml");
+Console.WriteLine("Started inserting xml in c#");
+
+//multithreadingCSV();
+await xmlConverter.LoadXML(@"C:\Users\Vulcan\source\repos\internship2026\Mikolaj\csvConverter\csvConverter\personal_data.xml");
 
 //converter.fetchData(0, 0, 0);
 
