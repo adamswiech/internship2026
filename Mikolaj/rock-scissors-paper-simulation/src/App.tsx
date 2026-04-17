14b2f6886be1148bb2109fa7a3f1a16d55368f02
mSajewiczz <mikolaj.sajewicz11@gmail.com>
2026-04-17 11:11:43 +0200

    Entity started to move and bounce from one border to another and again

--- Mikolaj/rock-scissors-paper-simulation/src/App.tsx
+++ Mikolaj/rock-scissors-paper-simulation/src/App.tsx
@@ -1,14 +1,66 @@
+import { useEffect, useRef, useState } from "react";
 import Entity from "./components/entity/Entity";
 
 function App() {
+  const entityRef = useRef(null);
+  const delay = 30;
+
+  useEffect(() => {
+    if (entityRef.current.style.marginTop == "") {
+      entityRef.current.style.marginTop = "480px";
+      entityRef.current.style.marginLeft = "980px";
+    }
+
+    const velocity = Math.floor(Math.random() * (5 - 1) + 1);
+
+    console.log(velocity);
+
+    const stopInterval = () => {
+      clearInterval(interval);
+    };
+
+    entityRef.current.up = true;
+    entityRef.current.down = false;
+
+    const interval = setInterval(() => {
+      entityRef.current.x = parseInt(entityRef.current.style.marginLeft);
+      entityRef.current.y = parseInt(entityRef.current.style.marginTop);
+
+      const entityVector = [entityRef.current.x, entityRef.current.y];
+
+      if (entityRef.current.up) {
+        entityRef.current.style.marginTop = `${entityVector[1] - velocity}px`;
+        entityRef.current.style.marginLeft = `${entityVector[0] - velocity}px`;
+      } else {
+        entityRef.current.style.marginTop = `${entityVector[1] + velocity}px`;
+        entityRef.current.style.marginLeft = `${entityVector[0] + velocity}px`;
+      }
+
+      // console.log(
+      //   `entityRef.current.style.marginTop = ${entityRef.current.style.marginTop}, entityRef.current.style.marginLeft = ${entityRef.current.style.marginLeft}`,
+      // );
+
+      // console.log(`x = ${entityRef.current.x}`);
+      // console.log(`y = ${entityRef.current.y}`);
+
+      if (parseInt(entityRef.current.y) <= 0) {
+        entityRef.current.up = false;
+        entityRef.current.down = true;
+        //based on vector value try to decide if you have to + or - velocity from value of margin
+      } else if (parseInt(entityRef.current.y) >= 480) {
+        entityRef.current.up = true;
+        entityRef.current.down = false;
+      }
+    }, delay);
+    
+  }, []);
+
   return (
     <>
       <div className="app-container">
         <h2>rock-scissors-paper simulation</h2>
         <div className="game-board">
-          <Entity role="🪨"/>
-          {/* <Entity role="✂️"/>
-          <Entity role="📃"/> */}
+          <Entity role="🪨" entityRef={entityRef} />
         </div>
       </div>
     </>
--- Mikolaj/rock-scissors-paper-simulation/src/components/entity/Entity.tsx
+++ Mikolaj/rock-scissors-paper-simulation/src/components/entity/Entity.tsx
@@ -1,46 +1,15 @@
-import { useRef, useState } from "react";
-
 interface Entity {
   role: string;
+  entityRef: any;
 }
 
-export default function Entity({ role }: Entity) {
-  const entityRef = useRef(null);
-  const delay = 1000;
-  const [distance, setDistance] = useState({
-    x: Math.random() * (2 - 1) + 1,
-    y: Math.random() * (2 - 1) + 1,
-  });
-
-  const entityMoveInterval = setInterval(() => {
-    entityRef.current.style.left = `${entityRef.current.getClientRects()[0].x + distance.x}px`;
-    entityRef.current.style.top = `${entityRef.current.getClientRects()[0].y + distance.y}px`;
-
-    console.log(entityRef.current.style.left);
-    console.log(entityRef.current.style.top);
-
-    if (parseInt(entityRef.current.style.left) < 75) {
-
-
-
-      clearInterval(entityMoveInterval);
-      console.log("test1");
-
-
-    } else if (parseInt(entityRef.current.style.top) < 55) {
-      clearInterval(entityMoveInterval);
-      console.log("test1");
-    } else if (parseInt(entityRef.current.style.left) > 1459) {
-      clearInterval(entityMoveInterval);
-      console.log("test1");
-    } else if (parseInt(entityRef.current.style.top) > 717) {
-      clearInterval(entityMoveInterval);
-      console.log("test1");
-    }
-  }, delay);
-
+export default function Entity({ role, entityRef }: Entity) {
   return (
-    <div className="entity" id="entity" ref={entityRef}>
+    <div
+      className="entity"
+      id="entity"
+      ref={entityRef}
+    >
       {role}
     </div>
   );
